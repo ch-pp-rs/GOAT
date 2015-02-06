fee8a5715564fbe9a3f45075e55a7f0bd354b49a
Ben Chaplin <ben.chaplin@burrows.yr.com>
2015-02-06 11:28:23 +0000

    Fix controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,6 +4,6 @@ angular.module('goatApp')
   .controller('MainCtrl', function ($scope, playerService) {
     playerService.getPlayerByUrl('http%3A%2F%2Fwww.basketball-reference.com%2Fplayers%2Fb%2Fbirdla01.html').
       then(function (data) {
-        $scope.player = data.data.results[0];
+        $scope.player = data.results[0];
       });
   });
